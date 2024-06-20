a91848dd882e00ecbe4d6db684c56362c0e8b1e2
shin <sato-shin-tm@ynu.jp>
2024-06-20 15:11:54 +0900

    #14 JQuery, CSS

--- scrollfade_noheight.js
+++ scrollfade_noheight.js
@@ -1,20 +1,18 @@
 //****************************************//
 // スクロールによるコンテンツのフェードイン //
-// を管理する関数（コンテンツ高さあり）     //
+// を管理する関数（コンテンツ高さなし）     //
 //****************************************//
 
 $(function() {
     // ウィンドウがスクロールされるたびに実行される関数
     $(window).scroll(function () {
       // ".js-scroll"というクラスを持つすべての要素に対して処理を行う
-      var list1 = [".js-scroll.-a", ".js-scroll.-b", ".js-scroll.-c", ".js-scroll.-d", ".js-scroll.-e", ".js-scroll.-f"];
-      var list2 = [".scroll-bottom.-a", ".scroll-bottom.-b", ".scroll-bottom.-c", ".scroll-bottom.-d", ".scroll-bottom.-e", ".scroll-bottom.-f"];
+      var list1 = [".mid_title.js-scroll.-a", ".mid_title.js-scroll.-b", 
+        ".mid_title.js-scroll.-c"];
       for(var i=0; i<list1.length; i++){
         $(list1[i]).each(function () {
           // 対象となる要素(js-scroll)の、ページの上からの位置を取得
           var jsscrollOffset = $(this).offset().top;
-          // 対象となる要素の底の部分(scroll-bottom)の、ページの上からの位置を取得
-          var scrollbottomOffset = $(list2[i]).offset().top;
           // ウィンドウの高さを取得
           var windowHeight = $(window).height();
           // 現在のスクロール位置 + ウィンドウの高さを計算
@@ -23,7 +21,7 @@ $(function() {
           var scrollbottomPosition = $(window).scrollTop();
     
           // 現在のスクロール位置が対象となる要素(js-scroll)の位置を超えた場合
-          if (scrollPosition > jsscrollOffset && scrollbottomPosition <= scrollbottomOffset) {
+          if (scrollPosition > jsscrollOffset && scrollbottomPosition <= jsscrollOffset) {
             // this = "js.scroll"クラスの要素に"js-active"というクラスを追加
             $(this).addClass("js-active");
           }
